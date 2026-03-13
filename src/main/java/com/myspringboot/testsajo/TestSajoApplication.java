888f661938fcc895f668717f3a2224688aef1240
변지수 <byounjisu0902@gmail.com>
2026-03-13 16:18:33 +0900

    FIRST COMMIT

---
+++ src/main/java/com/myspringboot/testsajo/TestSajoApplication.java
@@ -0,0 +1,13 @@
+package com.myspringboot.testsajo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSajoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSajoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myspringboot/testsajo/TestSajoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myspringboot.testsajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
